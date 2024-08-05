5f4e710935f726cf8abd021d9e5146b935f4ddb3
Dimserene <raul0520@google.com>
2024-08-05 15:20:15 +0800

    2024-08-05c

--- sh-version.sh
+++ sh-version.sh
@@ -1,8 +1,31 @@
-# Update local repository with the latest changes from the remote repository
+#!/bin/bash
+
+# Function to prompt user and wait for response
+prompt_to_continue() {
+    while true; do
+        read -p "$1 (press y to continue or n to abort): " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) echo "Aborting."; exit;;
+            * ) echo "Please answer y or n.";;
+        esac
+    done
+}
+
+# Function to prompt user to press any key to continue
+prompt_any_key() {
+    read -n 1 -s -r -p "$1 (press any key to continue)"
+    echo
+}
+
+# Pull the latest changes from the remote repository
 git pull
 
 # Update all submodules to their latest commit from the remote, merge them into the current branch
 git submodule update --remote --recursive --merge
+
+# Prompt user to continue editing CurrentVersion.txt
+prompt_to_continue "Ready to edit CurrentVersion.txt?"
 
 # Open the CurrentVersion.txt file for editing in the nano text editor
 nano ./CurrentVersion.txt
@@ -21,3 +44,6 @@ git commit -F Mods/ModpackUtil/CurrentVersion.txt
 
 # Push the committed changes to the remote repository
 git push
+
+# Prompt user after pushing changes
+prompt_any_key "Changes pushed to the remote repository."
