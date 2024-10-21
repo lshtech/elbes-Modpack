2a374c6d45113574ed6edc468de5f024684caae7
elbe <elbenews@gmail.com>
2024-10-20 19:01:48 -0500

    don't copy for now

--- Windows/SetupFull.bat
+++ Windows/SetupFull.bat
@@ -5,10 +5,10 @@ echo The installation will overwrite existing files in your Mods directory.
 echo *******************************************************
 pause
 
-git clone --recurse-submodules --remote-submodules https://github.com/Dimserene/Dimserenes-Modpack
+git clone --recurse-submodules --remote-submodules https://github.com/lshtech/elbes-Modpack
 
-echo Copying Mods folder to %appdata%\Balatro\Mods
-xcopy "Dimserenes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+rem echo Copying Mods folder to %appdata%\Balatro\Mods
+rem xcopy "elbes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
 
 echo Installation complete. Please ensure everything is working correctly.
 pause
--- Windows/UpdateFull.bat
+++ Windows/UpdateFull.bat
@@ -5,12 +5,12 @@ echo The installation will overwrite existing files in your Mods directory.
 echo *******************************************************
 pause
 
-git remote set-url origin https://github.com/Dimserene/Dimserenes-Modpack
+git remote set-url origin https://github.com/lshtech/elbes-Modpack
 git pull
 git submodule update --remote --recursive --merge
 
-echo Copying Mods folder to %appdata%\Balatro\Mods
-xcopy "Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+rem echo Copying Mods folder to %appdata%\Balatro\Mods
+rem xcopy "Mods" "%appdata%\Balatro\Mods" /w /e /h /i
 
 echo Update complete. Please ensure everything is working correctly.
 pause
