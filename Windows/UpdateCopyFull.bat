213320a9890e09dcfb77f8c81b8b6c8889fd6f45
elbe <elbenews@gmail.com>
2024-10-22 17:40:02 -0500

    add update with copy

---
+++ Windows/UpdateCopyFull.bat
@@ -0,0 +1,17 @@
+@echo off
+echo *******************************************************
+echo IMPORTANT: Please backup your current Mods folder before proceeding.
+echo The installation will overwrite existing files in your Mods directory.
+echo *******************************************************
+rem pause
+
+git remote set-url origin https://github.com/lshtech/elbes-Modpack
+git pull
+git submodule update --remote --recursive --merge
+
+echo Copying Mods folder to %appdata%\Balatro\Mods
+xcopy "Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+
+echo Update complete. Please ensure everything is working correctly.
+pause
+exit
