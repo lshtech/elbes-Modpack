a28677b7bace36480d02c579332068bd8e912425
elbe <elbenews@gmail.com>
2024-10-22 17:43:02 -0500

    add updated setup batch

---
+++ Windows/SetupCopyFull.bat
@@ -0,0 +1,14 @@
+@echo off
+echo *******************************************************
+echo IMPORTANT: Please backup your current Mods folder before proceeding.
+echo The installation will overwrite existing files in your Mods directory.
+echo *******************************************************
+pause
+
+git clone --recurse-submodules --remote-submodules https://github.com/lshtech/elbes-Modpack
+
+echo Copying Mods folder to %appdata%\Balatro\Mods
+xcopy "elbes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+
+echo Installation complete. Please ensure everything is working correctly.
+pause
